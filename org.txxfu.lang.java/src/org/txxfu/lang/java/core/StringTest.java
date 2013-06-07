5d67a5f16ed3b900a1850f11e77a0b803638169d
Jihua Wang <ivywjhua@gmail.com>
2013-06-07 18:30:13 +0800

    tidy up comment

--- org.txxfu.lang.java/src/org/txxfu/lang/java/core/StringTest.java
+++ org.txxfu.lang.java/src/org/txxfu/lang/java/core/StringTest.java
@@ -6,7 +6,7 @@ public class StringTest {
 
 	public static void main(String[] args) {
 		String str = "org.txxfu.lang.java.core";
-//		String subDir = str.replaceAll("\\.", "\\\\");
+		//String subDir = str.replaceAll("\\.", "\\\\");
 		String subDir = str.replace(".", File.separator);
 		System.out.println(subDir);
 	}

ce60f264cbdb3403ca37416df2b182ca8ce1362e
Jihua Wang <ivywjhua@gmail.com>
2013-06-09 17:55:53 +0800

    add ruby day3 src

---
+++ slsw/ruby/day3/blank.rb
@@ -0,0 +1,50 @@
+
+class NilClass
+	def blank?
+		true
+	end
+end
+
+class String
+	def blank?
+		self.size == 0
+	end
+end
+
+["", "person", nil].each do |element|
+	puts element unless element.blank?
+end
+
+class Numeric
+	def inches
+		self
+	end
+
+	def feet
+		self * 12.inches
+	end
+
+	def yards
+		self * 3.feet
+	end
+
+	def miles
+		self * 5280.feet
+	end
+
+	def back
+		self * -1
+	end
+
+	def forward
+		self
+	end
+end
+
+puts 10.miles
+puts 10.miles.back
+
+puts 2.feet
+puts 2.feet.forward
+
+
